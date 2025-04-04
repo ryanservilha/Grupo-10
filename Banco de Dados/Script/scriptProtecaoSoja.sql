85c94ca8fbf112deb0f061a1c2db80c92aef0471
FelipeLima24 <felipe.qlima@sptech.school>
2025-04-04 15:07:06 -0300

    AtualizaçoesTabela

--- Banco de Dados/Script/scriptProtecaoSoja.sql
+++ Banco de Dados/Script/scriptProtecaoSoja.sql
@@ -5,39 +5,43 @@ CREATE DATABASE protecaoSoja;
 USE protecaoSoja;
 
 -- CRIAÇÃO DA TABELA DO CLIENTE
+
 CREATE TABLE enderecoEmpresa (
-	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
-    logradouro VARCHAR(100),
-    numero VARCHAR(6),
-    cidade VARCHAR(45),
-    estado CHAR(2),
-    pais VARCHAR(45)
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    numero VARCHAR(6) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    pais VARCHAR(45) NOT NULL
 );
+-- CRIAÇÂO DA TABELA EMPRESA 
 
 CREATE TABLE empresa (
-	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-	razaoSocial VARCHAR(45),
-    cnpj CHAR(14),
-    emailEmpresa VARCHAR(45),
-    nomeUsuario VARCHAR(45),
-    senhaEmpresa VARCHAR(45),
-    telefone CHAR(11),
-    fkEndereco INT UNIQUE,
-    CONSTRAINT fkEmpresaEndereco
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	razaoSocial VARCHAR(45) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    emailEmpresa VARCHAR(45) NOT NULL,
+    nomeUsuario VARCHAR(45) NOT NULL,
+    senhaEmpresa VARCHAR(45) NOT NULL,
+    telefone CHAR(11) NOT NULL,
+    fkEndereco INT UNIQUE NOT NULL,
+    CONSTRAINT fkEmpresaEndereco 
     FOREIGN KEY (fkEndereco)
     REFERENCES enderecoEmpresa(idEndereco)
 );
+-- CRIAÇÃO DA TABELA SETOR FUNCIONARIO 
 
 CREATE TABLE setorFuncionario (
-	idSetor INT PRIMARY KEY AUTO_INCREMENT,
-    nomeSetor VARCHAR(45)
+	idSetor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nomeSetor VARCHAR(45) NOT NULL
 );
+-- CRIAÇÃO DA TABELA FUNCIONARIO EMPRESA 
 
 CREATE TABLE funcionarioEmpresa (
-	idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
-    nomeFuncionario VARCHAR(60),
-    fkSetor INT,
-    fkGerente INT,
+	idFuncionario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nomeFuncionario VARCHAR(60) NOT NULL,
+    fkSetor INT NOT NULL,
+    fkGerente INT NOT NULL,
     CONSTRAINT fkSetorFuncionario 
     FOREIGN KEY (fkSetor) 
     REFERENCES setorFuncionario(idSetor),
@@ -46,13 +50,15 @@ CREATE TABLE funcionarioEmpresa (
     REFERENCES funcionarioEmpresa(idFuncionario)
 );
 
+-- CRIAÇÃO DA TABELA SENSOR EMPRESA
+
 CREATE TABLE sensorEmpresa (
-	idSensor INT PRIMARY KEY AUTO_INCREMENT,
-    nomeSensor VARCHAR(20),
-    statusSensor VARCHAR(7),
-    localidadeSensor VARCHAR(45),
-    fkEmpresa INT,
-    fkResponsavelSensor INT,
+	idSensor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nomeSensor VARCHAR(20) NOT NULL,
+    statusSensor VARCHAR(7) NOT NULL,
+    localidadeSensor VARCHAR(45) NOT NULL,
+    fkEmpresa INT NOT NULL,
+    fkResponsavelSensor INT NOT NULL,
     CONSTRAINT chkStatusSensor	
     CHECK (statusSensor in ('ativo', 'inativo')),
     CONSTRAINT fkSensorEmpresa
@@ -63,11 +69,13 @@ CREATE TABLE sensorEmpresa (
     REFERENCES funcionarioEmpresa(idFuncionario)
 );
 
+-- CRIAÇÃO DA TABELA DADO SENSOR 
+
 CREATE TABLE dadoSensor (
-	idDadoSensor INT PRIMARY KEY AUTO_INCREMENT,
-	dadoSensor DECIMAL(3,1),
-    dataHoraEmissao TIMESTAMP,
-    fkSensor INT,
+	idDadoSensor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	dadoSensor DECIMAL(3,1) NOT NULL,
+    dataHoraEmissao TIMESTAMP NOT NULL,
+    fkSensor INT NOT NULL,
     CONSTRAINT fkSensorDado
     FOREIGN KEY (fkSensor)
     REFERENCES sensorEmpresa(idSensor)
